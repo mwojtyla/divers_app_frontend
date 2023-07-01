7e0aee42ad48037b9d3b96f2086e3375bb71b7cf
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-07-01 23:19:11 +0200

    Initiate commit

---
+++ src/main/java/com/diver/DiverApplication.java
@@ -0,0 +1,13 @@
+package com.diver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diver/DiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
