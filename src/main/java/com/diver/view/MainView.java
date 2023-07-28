34a71163e315386ed4acef42b4301127bf325e12
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-07-28 11:33:25 +0200

    Modified MainView class

--- src/main/java/com/diver/view/MainView.java
+++ src/main/java/com/diver/view/MainView.java
@@ -1,13 +1,14 @@
-package com.diver;
+package com.diver.view;
 
+import com.diver.api.client.DiverUserClient;
+import com.diver.api.config.UserContext;
+import com.diver.dto.UserDtoToAuthorised;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -15,7 +16,19 @@ import com.vaadin.flow.router.Route;
 
 @Route("main-view")
 public class MainView extends VerticalLayout {
-    public MainView() {
+
+    private final DiverUserClient diverUserClient;
+
+    private UserContext userContext;
+    private TextField login = new TextField("Login");
+    private PasswordField password = new PasswordField("Password");
+    private Button logIn = new Button("Log in");
+    private Button signUp = new Button("Sign up");
+
+
+    public MainView(DiverUserClient diverUserClient, UserContext userContext) {
+        this.diverUserClient = diverUserClient;
+        this.userContext = userContext;
         setSizeFull();
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
@@ -23,26 +36,31 @@ public class MainView extends VerticalLayout {
         add(
                 new H1("DIVER'S APPLICATION"),
                 new H2("Start your diving adventure!"),
-                new TextField("Login"),
-                new PasswordField("Password"),
-                new Button("Log in", event ->
-                        UI.getCurrent().navigate("home")),
+                login,
+                password,
+                logIn,
                 new H4("Don't have an account?"),
-                new Button("Sign up", event ->
-                        UI.getCurrent().navigate("registration"))
-                );
+                signUp
+        );
 
+        logIn.addClickListener(clickEvent -> {
+            if (diverUserClient.isAuthorised(UserDtoToAuthorised.builder()
+                    .login(login.getValue())
+                    .password(password.getValue())
+                    .build())) {
 
-        //VerticalLayout layout = new VerticalLayout();
+                this.userContext.setUserId(diverUserClient.getUserIdByLoginAndPassword(UserDtoToAuthorised.builder()
+                        .login(login.getValue())
+                        .password(password.getValue())
+                        .build()));
 
-/*
+                UI.getCurrent().navigate("home");
+            } else {
+                Notification.show("Wrong login or password!");
+            }
+        });
 
-            layout.add(new Button("LogIn"));
-            layout.add(new Button("SignIn"));
-
-            this.setClassName("basic-layouts-example");
-
-            this.add(layout);*/
+        signUp.addClickListener(clickEvent ->
+                UI.getCurrent().navigate("registration"));
     }
-
 }
